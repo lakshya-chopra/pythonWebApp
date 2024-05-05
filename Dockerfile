82bd3945578b6794c24faeae53a7ee9c9fa6baec
root <root@LAPTOP-BAFAU0RA>
2024-05-05 23:15:49 +0530

    update

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN pip3 install flask
 
 COPY app .
 
-EXPOSE 8008
+EXPOSE 80
 
 CMD ["python", "app.py", "runserver", "0.0.0.0:8008"]
--- app/app.py
+++ app/app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/')
 def hello_world():
     return "Hello World! How are you doing?"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8008, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
