97a2ff3c4d01622101e0ea1d11c6b16786e84c32
Lakshya Chopra <77010972+lakshya-chopra@users.noreply.github.com>
2024-05-06 00:20:25 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World! How are you doing? Doing fine!!! TEST"
+    return "Hello World! How are you doing? Doing fine!!! TEST TEST2"
 
 
 if __name__ == "__main__":
